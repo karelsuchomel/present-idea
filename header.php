c9643839e1842362abfbc566d486f5246c788fd6
Karel Suchomel <karelsuchomel@windowslive.com>
2017-06-23 17:25:44 +0200

    commiting files from orginal directory

---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+  <head>
+    <title><?php bloginfo('name');?></title>
+    <meta charset="<?php bloginfo('charset');?>">
+    <meta name="description" content="idea-presenter-wp-theme">
+    <meta name="keywords" content="Simplistic WordPress theme, Present papers, Present idea">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Links-->
+    <!-- Fonts-->
+    <!-- Open Sans Light 300, Light Italic, Normal 400, Semi-bold 600, Bold 700-->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,600,700&amp;subset=latin-ext" rel="stylesheet"> 
+    <!-- JS  -->
+    <!-- Include google RECAPTCHA API -->
+    <script src='https://www.google.com/recaptcha/api.js'></script>
+    <?php wp_head(); ?>
+  </head>
+  <body <?php body_class(); ?>>
+
+    <!-- site-header -->
